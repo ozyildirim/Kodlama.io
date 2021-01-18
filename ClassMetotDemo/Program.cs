d57b64f319ddee3882213b76c695d81e9f002fa8
ozyildirim <kutay9622@gmail.com>
2021-01-18 17:52:42 +0300

    .

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -7,7 +7,7 @@ namespace ClassMetotDemo
         static void Main(string[] args)
         {
             MusteriManager musteriManager = new MusteriManager();
-            Musteri ozge = new Musteri() { Id = 1, Ad = "Özge", Soyad = "Kiracı", TCnumara = 111, Adres = "Istanbul", Bakiye = 1000 };
+            Musteri ozge = new Musteri() { Id = 1, Ad = "Işıl", Soyad = "Alben", TCnumara = 111, Adres = "Istanbul", Bakiye = 1000 };
             Musteri mahmut = new Musteri() { Id = 2, Ad = "Mahmut", Soyad = "Tuncer", TCnumara = 112, Adres = "Ankara", Bakiye = 300 };
             musteriManager.MusteriEkle(ozge);
             musteriManager.MusteriEkle(mahmut);
